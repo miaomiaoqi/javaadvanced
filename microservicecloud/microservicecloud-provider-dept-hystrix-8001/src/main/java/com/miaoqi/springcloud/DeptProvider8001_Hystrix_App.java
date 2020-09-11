c80360de7157314ad60c6bf44e69d195aac61abe
miaoqi <miaoqi3@xdf.cn>
2020-09-12 01:44:22 +0800

    update blog

---
+++ microservicecloud/microservicecloud-provider-dept-hystrix-8001/src/main/java/com/miaoqi/springcloud/DeptProvider8001_Hystrix_App.java
@@ -0,0 +1,19 @@
+package com.miaoqi.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+@SpringBootApplication
+@EnableEurekaClient // Eureka客户端
+@EnableDiscoveryClient // 服务发现
+@EnableHystrix // 对Hystrix熔断器的支持
+public class DeptProvider8001_Hystrix_App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeptProvider8001_Hystrix_App.class, args);
+    }
+
+}
