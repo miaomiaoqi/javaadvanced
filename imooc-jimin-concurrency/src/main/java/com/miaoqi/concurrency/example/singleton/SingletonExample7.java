f3d3b76059fca2768e95e096ffc97c380332e513
miaoqi <miaoqi3@xdf.cn>
2023-11-20 16:21:26 +0800

    update blog

---
+++ imooc-jimin-concurrency/src/main/java/com/miaoqi/concurrency/example/singleton/SingletonExample7.java
@@ -0,0 +1,35 @@
+package com.miaoqi.concurrency.example.singleton;
+
+/**
+ * 安全发布对象
+ * 枚举生成单例对象, 性能与安全都是最好的
+ *
+ * @author miaoqi
+ * @date 2018/11/8
+ */
+public class SingletonExample7 {
+
+    private SingletonExample7() {
+
+    }
+
+    public static SingletonExample7 getInstance() {
+        return Singleton.INSTANCE.getInstance();
+    }
+
+    public enum Singleton {
+        INSTANCE;
+
+        private SingletonExample7 singleton;
+
+        // JVM保 证枚举的构造函数绝对只调用一次
+        private Singleton() {
+            singleton = new SingletonExample7();
+        }
+
+        public SingletonExample7 getInstance() {
+            return singleton;
+        }
+    }
+
+}
---
+++ imooc-jimin-concurrency/src/main/java/com/miaoqi/concurrency/example/threadpool/ThreadPoolExample3.java
@@ -0,0 +1,29 @@
+package com.miaoqi.concurrency.example.threadpool;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 单一线程的线程池
+ *
+ * @author miaoqi
+ * @date 2019/11/22
+ */
+@Slf4j
+public class ThreadPoolExample3 {
+
+    public static void main(String[] args) {
+        // 单线程的, 所以结果是顺序的
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            executorService.submit(() ->
+                    log.info("task: {}", index)
+            );
+        }
+        executorService.shutdown();
+    }
+
+}
